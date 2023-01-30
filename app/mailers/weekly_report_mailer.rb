9799d89b384a4676d44da2f06591e7e6810109f8
kiburei <gooddeeds17@gmail.com>
2023-01-30 13:52:15 +0300

    body

--- app/mailers/weekly_report_mailer.rb
+++ app/mailers/weekly_report_mailer.rb
@@ -2,6 +2,17 @@ class WeeklyReportMailer < ApplicationMailer
   default from: 'gooddeeds17@gmail.com'
 
   def weekly_report_email
-    mail(:to => 'akiburei@gmail.com', :subject => "Attendance Weekly Report: #{(Date.today - 1.week).beginning_of_week..(Date.today - 1.week).end_of_week}")
+    mail(
+      :to => 'akiburei@gmail.com',
+      :subject => "Attendance Weekly Report: #{(Date.today - 1.week).beginning_of_week..(Date.today - 1.week).end_of_week}",
+      :body => "<p>Hi</p>
+        <p>Please click the following link to download the weekly attendance report</p>
+        <ul>
+          <li><a href='http://172.18.1.37:3000/weekly_report.xlsx' target='blank'>Report for: </a>#{(Date.today - 1.week).beginning_of_week..(Date.today - 1.week).end_of_week}</li>
+        </ul>
+          <p>
+            Regards
+          </p>"
+    )
   end
 end
