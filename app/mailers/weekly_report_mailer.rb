ada647f893551099ec41b89ad2045bcb9d408cbb
kiburei <gooddeeds17@gmail.com>
2023-01-30 13:46:02 +0300

    Sanitize

--- app/mailers/weekly_report_mailer.rb
+++ app/mailers/weekly_report_mailer.rb
@@ -2,6 +2,6 @@ class WeeklyReportMailer < ApplicationMailer
   default from: 'gooddeeds17@gmail.com'
 
   def weekly_report_email
-    mail(:to => 'akiburei@gmail.com', :subject => "Weekly Report: #{Date.today.beginning_of_week..Date.today.end_of_week}")
+    mail(:to => 'akiburei@gmail.com', :subject => "Attendance Weekly Report: #{(Date.today - 1.week).beginning_of_week..(Date.today - 1.week).end_of_week}")
   end
 end
